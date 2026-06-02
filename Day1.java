3f1d240cc9d25f334bcd34b907b58916959f0cb2
Navya Jede <navyajede2005@gmail.com>
2026-06-02 10:47:29 +0530

    Day1

--- Day1.java
+++ Day1.java
@@ -4,6 +4,10 @@ public class Day1 {
         int[] num= {10,39,58,45,38};
         int ans= LinearSearch(num, 78);
         System.out.println(ans);
+
+        String name ="Navya Nandha";
+        char target='y';
+        System.out.println(StringSearch(name, target));
         
     }
 
@@ -21,4 +25,18 @@ public class Day1 {
         return -1;
 
     }
+
+    static String StringSearch(String str , char target){
+        if(str.length()==0){
+            return "not found";
+
+        }
+        for(int i=0;i<str.length();i++){
+            if(target==str.charAt(i)){
+                return String.valueOf(str.charAt(i));
+            }
+        }
+        return "not found";
+    }
 }
+
