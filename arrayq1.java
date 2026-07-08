9b8425fe717fc05c1b00023b72c11dfc502b56b1
Navya Jede <navyajede2005@gmail.com>
2026-07-08 19:23:30 +0530

    arrayq1

---
+++ arrayq1.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class arrayq1 {
+    public static void main(String [] args){
+        int[] arr = new int[5];
+        Scanner sc = new Scanner(System.in);
+        for(int i=0;i<arr.length;i++){
+            arr[i]=sc.nextInt();
+            if(i%2!=0){
+                arr[i]=arr[i]*2;
+
+            }
+            else{
+                arr[i]=arr[i]+10;
+            }
+        }
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+
+        }
+
+         
+    }
+}
