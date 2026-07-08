feab9092570613edc0053347006b293855aea457
Navya Jede <navyajede2005@gmail.com>
2026-07-08 21:24:47 +0530

    secmaxinarray

--- secmaxinarray.java
+++ secmaxinarray.java
@@ -1,6 +1,22 @@
 public class secmaxinarray {
     public static void main(String[] args) {
-        
+        int[] arr = {10,30,657,677,677};
+        int max=arr[0];
+        int max2=Integer.MIN_VALUE;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>max){
+                max2=max;
+                max=arr[i];
+                
+            }
+            if(arr[i]>max2 && arr[i]!=max){
+                max2=arr[i];
+            }
+
+            
+
+        }
+        System.out.println(max2);
     }
     
 }
